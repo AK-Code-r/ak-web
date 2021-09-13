380e9c43659bef6925cbb1ec5f196d0928134a64
AK-Code-r <86775347+AK-Code-r@users.noreply.github.com>
2021-09-13 16:38:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Coder", "App Developer"],
+        strings: ["a Coder", "an App Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Coder", "App Developer"],
+        strings: ["a Coder", "an App Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
